d30ebdbc6d6881f82aa14a1d21aba3ecfe7b7c15
liangyishi9527 <146693168+liangyishi9527@users.noreply.github.com>
2025-01-07 17:13:35 +0800

    Update sendNotification.js
    Signed-off-by: liangyishi9527 <146693168+liangyishi9527@users.noreply.github.com>

--- script/sendNotification.js
+++ script/sendNotification.js
@@ -5,6 +5,7 @@ import FormData from 'form-data';
 const platform = process.env.INPUTS_PLATFORM;
 const name = process.env.INPUTS_NAME;
 const chat_id = process.env.INPUTS_CHAT_ID || '-1002257173670'
+const bot_token = process.env.INPUT_BOT_TOKEN || '7518333944:AAFwQrQIcU4HriivCciwe70yk-djhdp8wI'
 
 let fileName;
 if (platform === 'windows-latest') {
@@ -19,7 +20,7 @@ const message = `构建完成: ${name} for ${platform} 已生成: ${fileName}`;
 
 const sendMessage = async () => {
   try {
-    await axios.post(`https://api.telegram.org/bot7518333944:AAFwQrQIcU4HriivCciwe70yk-djhdp8wIs/sendMessage`, {
+    await axios.post(`https://api.telegram.org/bot${bot_token}/sendMessage`, {
       chat_id,
       text: message,
     });
@@ -29,7 +30,7 @@ const sendMessage = async () => {
     form.append('chat_id', chat_id);
     form.append('document', fs.createReadStream(`node_modules/pake-cli/output/${fileName}`));
 
-    await axios.post(`https://api.telegram.org/bot7518333944:AAFwQrQIcU4HriivCciwe70yk-djhdp8wIs/sendDocument`, form, {
+    await axios.post(`https://api.telegram.org/bot${bot_token}/sendDocument`, form, {
       headers: form.getHeaders(),
     });
   } catch (error) {
