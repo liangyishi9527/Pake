169a6f4d70f0b75dbbc7143c7da1897aef12b201
ac009527 <100550131+ac009527@users.noreply.github.com>
2025-01-07 17:16:06 +0800

    Update sendNotification.js
    Signed-off-by: ac009527 <100550131+ac009527@users.noreply.github.com>

--- script/sendNotification.js
+++ script/sendNotification.js
@@ -5,7 +5,7 @@ import FormData from 'form-data';
 const platform = process.env.INPUTS_PLATFORM;
 const name = process.env.INPUTS_NAME;
 const chat_id = process.env.INPUTS_CHAT_ID || '-1002257173670'
-const bot_token = process.env.INPUT_BOT_TOKEN || '7518333944:AAFwQrQIcU4HriivCciwe70yk-djhdp8wI'
+const bot_token = process.env.INPUT_BOT_TOKEN || '7518333944:AAFwQrQIcU4HriivCciwe70yk-djhdp8wIs'
 
 let fileName;
 if (platform === 'windows-latest') {
